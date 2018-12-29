73079de1fb65eb1ba984a0c3901f482096f664e9
HarshiniGadige <36823079+HarshiniGadige@users.noreply.github.com>
2018-12-29 16:22:10 -0600

    Add files via upload

---
+++ Weapon.h
@@ -0,0 +1,16 @@
+//
+//  Weapon.h
+//  PirateAdventure
+//
+//  Created by Avinash P08 on 14/09/17.
+//  Copyright © 2017 Infosys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Weapon : NSObject
+
+@property (nonatomic) int damage;
+@property (strong, nonatomic) NSString *name;
+
+@end
---
+++ Weapon.m
@@ -0,0 +1,13 @@
+//
+//  Weapon.m
+//  PirateAdventure
+//
+//  Created by Avinash P08 on 14/09/17.
+//  Copyright © 2017 Infosys. All rights reserved.
+//
+
+#import "Weapon.h"
+
+@implementation Weapon
+
+@end
